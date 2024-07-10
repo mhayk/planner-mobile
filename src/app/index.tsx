6baec07918d59d3a4133e592e45aca9f443ccd71
Mhayk Whandson <hi@mhayk.com>
2024-07-10 10:22:25 +0100

    feat: install the google font and create a new loading component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 export default function Index() {
     return (
-        <View className=''>
-            <Text>Hi Mhayk!</Text>
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-zinc-400'>Invite your friends and plan your next trip</Text>
         </View>
     )
 }
