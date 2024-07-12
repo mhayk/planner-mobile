b96d97ba0d920eb3ad9b87ed2cefbd92d949144d
Mhayk Whandson <hi@mhayk.com>
2024-07-12 11:02:51 +0100

    feat: button receives the className as parameter

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -17,14 +17,21 @@ type ButtonProps = TouchableOpacityProps & {
 
 const ThemeContext = createContext<{ variant?: Variants }>({})
 
-function Button({ variant = "primary", children, isLoading, ...rest }: ButtonProps) {
+function Button({
+    variant = "primary",
+    children,
+    isLoading,
+    className,
+    ...rest
+}: ButtonProps) {
     return <TouchableOpacity
         className={clsx(
-            "w-full h-11 flex-row  items-center justify-center rounded-lg gap-2",
+            "h-11 flex-row  items-center justify-center rounded-lg gap-2",
             {
                 "bg-lime-300": variant === "primary",
                 "bg-zinc-800": variant === "secondary",
-            }
+            },
+            className
         )}
         activeOpacity={0.7}
         disabled={isLoading}
@@ -43,7 +50,7 @@ function Title({ children }: TextProps) {
             "text-base font-semibold",
             {
                 "text-lime-950": variant === "primary",
-                "text-lime-200": variant === "secondary",
+                "text-zinc-200": variant === "secondary",
             }
         )}
     >
